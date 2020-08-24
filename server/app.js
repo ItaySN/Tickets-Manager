6bde512b631fd9b515f5731c95cb55c144b3f9f4
ItaySN <itayfic@gmail.com>
2020-08-24 19:11:48 +0300

    Create the POST methods.
    [POST] api/tickets/:ticketId/done - Sets done property to true for the given ticketId
    [POST] api/tickets/:ticketId/undone - Sets done property to false for the given ticketId

--- server/app.js
+++ server/app.js
@@ -20,3 +20,59 @@ app.get('/api/tickets/', async (req, res) => {
   }
 });
 
+app.post('/api/tickets/:ticketId/done/', async (req, res) => {
+  const data = await fs.readFile('./data.json');
+  let ticketsJson = JSON.parse(data);
+  try {
+    const tickectMap = ticketsJson.map((item) => {
+      if (item.id === req.params.ticketId) {
+        item.done = true;
+      }
+      return item;
+    });
+    ticketsJson = JSON.stringify(tickectMap);
+    await fs.writeFile('./data.json', ticketsJson);
+    res.send(`ticket ${req.params.ticketId} has done`);
+  } catch (e) {
+    res.send(`You got an error ${e.message}`);
+  }
+});
+
+// app.post('/api/tickets/:ticketsId/undone/', async (req, res) => {
+//   const data = await fs.readFile('./data.json');
+//   const ticketsJson = JSON.parse(data);
+//   let i = 0;
+//   let tempCheck = true;
+//   try {
+//     do {
+//       if (ticketsJson[i].id === req.params.ticketId) {
+//         ticketsJson[i].done = false;
+//         tempCheck = false;
+//       }
+//       i += 1;
+//     } while (tempCheck || i < ticketsJson.lengh);
+//     JSON.stringify(ticketsJson);
+//     await fs.writeFile('./data.json', ticketsJson);
+//     res.send(`ticket ${req.params.ticketId} has undone`);
+//   } catch (e) {
+//     res.send(`You got an error ${e.message}`);
+//   }
+// });
+
+app.post('/api/tickets/:ticketId/undone/', async (req, res) => {
+  const data = await fs.readFile('./data.json');
+  let ticketsJson = JSON.parse(data);
+  try {
+    const tickectMap = ticketsJson.map((item) => {
+      if (item.id === req.params.ticketId) {
+        item.done = false;
+      }
+      return item;
+    });
+    ticketsJson = JSON.stringify(tickectMap);
+    await fs.writeFile('./data.json', ticketsJson);
+    res.send(`ticket ${req.params.ticketId} has undone`);
+  } catch (e) {
+    res.send(`You got an error ${e.message}`);
+  }
+});
