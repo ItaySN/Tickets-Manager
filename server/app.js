e294f67f8959987ddd147b791a1c8324cad35303
ItaySN <itayfic@gmail.com>
2020-08-24 14:21:08 +0300

    Method GET
    returns an array of tickets from saved in server/data.json.

--- server/app.js
+++ server/app.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express();
 
+const fs = require('fs');
+
 module.exports = app;
 
 app.use(express.json());
+
+
+app.get('/api/tickets', async (req, res) => {
+  const data = await fs.readFile('data.json');
+  const tickets = JSON.parse(data);
+  res.send(tickets);
+});
