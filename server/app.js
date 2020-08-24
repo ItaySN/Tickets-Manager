0f392d118ccf36e58c17cfb0196e9b78a0632d67
ItaySN <itayfic@gmail.com>
2020-08-24 19:10:47 +0300

    Create the GET METHODS
    [GET] api/tickets - returns an array of tickets from saved in server/data.json. If called with query param searchText the API will filter only tickets that have a title including a case-insensitive version of the searchText param

--- server/app.js
+++ server/app.js
@@ -2,15 +2,21 @@ const express = require('express');
 
 const app = express();
 
-const fs = require('fs');
+const fs = require('fs').promises;
 
 module.exports = app;
 
 app.use(express.json());
 
-
-app.get('/api/tickets', async (req, res) => {
-  const data = await fs.readFile('data.json');
+app.get('/api/tickets/', async (req, res) => {
+  const data = await fs.readFile('./data.json');
   const tickets = JSON.parse(data);
-  res.send(tickets);
+  if (!req.query.searchText) {
+    res.send(tickets);
+  } else {
+    // eslint-disable-next-line max-len
+    const filterData = tickets.filter((ticket) => ticket.title.toLowerCase().includes(req.query.searchText));
+    res.send(filterData);
+  }
 });
+
